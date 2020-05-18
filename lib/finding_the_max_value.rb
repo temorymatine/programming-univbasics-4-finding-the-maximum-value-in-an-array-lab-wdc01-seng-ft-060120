1d58182f3c4b36d5d0156b4523c9dc9ac92ac612
Matine temory <temorymatine@gmail.com>
2020-05-18 02:17:47 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,9 +2,10 @@ def find_max_value(array)
   count = 0 
  while count < array.length do
     if array[count] > array[count+=1]
-      puts "hello"
+      array[count] == max_value
   end
     count+=1
     
   end
+  puts max_value
 end
